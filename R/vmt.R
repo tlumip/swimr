4b05e695b15beb6f4415dbf8916d9fc2e49c4e8f
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-07-17 11:02:31 -0600

    Add vht plot.

--- R/vmt.R
+++ R/vmt.R
@@ -129,3 +129,68 @@ plot_pct_cong <- function(db, color_var = "MPO", color_levels = NULL,
     theme_bw()
 
 }
+
+
+
+#' Plot VHT over time.
+#'
+#' This function plots the sum of the daily vehicle travel time, colored and
+#' faceted by arbitrary variables in the link or zone table.
+#'
+#' @param db The scenario database.
+#' @param color_var The variable to use in coloring the data.
+#' @param color_levels A character vector giving the levels of the coloring
+#'   variable to use. Levels not called are dropped from the plot; default is
+#'   \code{NULL}, meaning print all level.
+#'
+#' @return A ggplot2 figure object.
+#'
+#' @export
+#' @impor dplyr, ggplot2
+#'
+plot_vht <- function(db, color_var = "MPO", color_levels = NULL){
+
+  # Get lookup table of zones to grouping variable.
+  grouping <- tbl(db, "ALLZONES") %>%
+    select_("Azone", color_var) %>%
+    rename(AZONE = Azone) %>%
+    rename_("color_var" = color_var)
+
+  # If no levels are specified, show all but external stations.
+  if(is.null(color_levels)){
+    a <- grouping %>%
+      collect() %>%
+      filter(color_var != "EXTSTA")
+
+    color_levels = names(table(a$color_var))
+  }
+
+  links <- tbl(db, "LINK_DATA") %>%
+    select(AZONE, TSTEP, DAILY_TIME_AUTO) %>%
+    left_join(grouping, by = "AZONE") %>%
+
+    # filter out regions you don't want
+    filter(color_var %in% color_levels) %>%
+
+    # calculate total by year and group
+    group_by(TSTEP, color_var) %>%
+    summarise(vht = sum(DAILY_TIME_AUTO)) %>%
+
+    # bring it locally
+    collect() %>%
+    ungroup() %>%
+    mutate(TSTEP = as.numeric(TSTEP))
+
+  p <- ggplot(
+    links,
+    aes(x = TSTEP + 1990, y = vht, color = color_var)
+  ) +
+    geom_path() +
+    scale_y_log10() +
+    scale_color_discrete(color_var)
+
+  p +
+    xlab("Year") + ylab("Vehicle Hours Traveled") +
+    theme_bw()
+
+}
