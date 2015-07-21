09e4dc041a43833afa3d84b28177ebc2d4466ba9
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-07-21 16:55:51 -0400

    Stop write to data/
    Don't need to do this every time you build the package.

--- R/ref_counts.R
+++ R/ref_counts.R
@@ -1,8 +1,7 @@
 # Read base year counts and save
-
 library(readr)
 
 ref_counts <- read_csv("data-raw/reference-aadt-counts.csv",
                        col_types = "cnncc")
 
-devtools::use_data(ref_counts)
+# devtools::use_data(ref_counts, overwrite = TRUE)
