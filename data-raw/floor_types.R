e3d46f26cc1537a803680a03c6340d460ecb5414
Greg Macfarlane <gregmacfarlane@gmail.com>
2015-07-28 18:38:35 -0400

    Add floor type conversion data frame.

---
+++ data-raw/floor_types.R
@@ -0,0 +1,11 @@
+floor_types <- data_frame(
+  commodity = types,
+  floor_type = c(
+    "Retail", "Multifamily", "Services", "Industrial",
+    "Institutional", "Institutional", "Institutional",
+    "Industrial", "Multifamily", "Multifamily",
+    "Commercial", "Retail", "Multifamily", "Singlefamily",
+    "Singlefamily", "Industrial"
+  )
+)
+
