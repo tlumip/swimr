3fb017efb03dd158fa0a8f07ddc4fa3b098051fa
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-08-12 10:11:57 -0400

    Fix bug where VMT plot is only showing link volumes, not VMT.

--- R/vmt.R
+++ R/vmt.R
@@ -11,49 +11,67 @@
 #'
 #' @return A ggplot2 figure object.
 #'
-#' @import dplyr ggplot2
+#' @import dplyr ggplot2 dplyrExtras
+#'
 #'
 #' @export
-plot_vmt <- function(db, color_var = "MPO", color_levels = NULL){
+plot_vmt <- function(db, facet_var = "MPO", facet_levels = NULL){
 
   # Get lookup table of zones to grouping variable.
   grouping <- tbl(db, "ALLZONES") %>%
-    select_("Azone", color_var) %>%
+    select_("Azone", facet_var) %>%
     rename(AZONE = Azone) %>%
-    rename_("color_var" = color_var)
+    rename_("facet_var" = facet_var)
+
+  # Link distances
+  link_dist <- links %>%
+    # reverse link distance
+    mutate_if(
+      order == 2,
+      hold = FROMNODENO,
+      FROMNODENO = TONODENO,
+      TONODENO = hold,
+      LENGTH = R_LENGTH
+    ) %>%
+    select(FROMNODENO, TONODENO, LENGTH)
+
 
   # If no levels are specified, show all but external stations.
-  if(is.null(color_levels)){
+  if(is.null(facet_levels)){
     a <- grouping %>%
       collect() %>%
-      filter(color_var != "EXTSTA")
+      filter(facet_var != "EXTSTA")
 
-    color_levels = names(table(a$color_var))
+    facet_levels = names(table(a$facet_var))
   }
 
-  links <- tbl(db, "LINK_DATA") %>%
-    select(AZONE, TSTEP, DAILY_VOL_TOTAL) %>%
+  link_vmt <- tbl(db, "LINK_DATA") %>%
+    select(AZONE, ANODE, BNODE, TSTEP, PLANNO, DAILY_VOL_TOTAL) %>%
     left_join(grouping, by = "AZONE") %>%
 
     # filter out regions you don't want
-    filter(color_var %in% color_levels) %>%
+    filter(facet_var %in% facet_levels) %>%
+    collect()  %>%
+
+    # get link distance from table
+    left_join(link_dist, by = c("ANODE" = "FROMNODENO", "BNODE" = "TONODENO")) %>%
 
     # calculate total by year and group
-    group_by(TSTEP, color_var) %>%
-    summarise(vmt = sum(DAILY_VOL_TOTAL)) %>%
+    group_by(TSTEP, facet_var, PLANNO) %>%
+    summarise(vmt = sum(DAILY_VOL_TOTAL * LENGTH, na.rm = TRUE)) %>%
 
     # bring it locally
-    collect() %>%
     ungroup() %>%
     mutate(TSTEP = as.numeric(TSTEP))
 
   p <- ggplot(
-    links,
-    aes(x = TSTEP + 1990, y = vmt, color = color_var)
+    link_vmt,
+    aes(x = TSTEP + 1990, y = vmt, color = factor(PLANNO))
   ) +
     geom_path() +
     scale_y_log10() +
-    scale_color_discrete(color_var)
+    facet_wrap(~ facet_var) +
+    scale_color_discrete("Facility Type")
 
   p +
     xlab("Year") + ylab("Vehicle Miles Traveled") +
