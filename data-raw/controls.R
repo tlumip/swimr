8b88c286d09a52004e2addeab4f7ced46dab8297
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-09-21 13:22:14 -0400

    Add historical population data.

--- data-raw/controls.R
+++ data-raw/controls.R
@@ -13,3 +13,14 @@ pop <- as.data.frame(Pop.CoYr) %>% tbl_df() %>%
 county_controls <- pop %>% left_join(emp) %>%
   mutate(year = as.numeric(year)) %>%
   gather(var, y, population:employment)
+
+# historical population for Oregon counties
+historical_pop <- read_excel(
+  "~/Documents/projects/swim/Oregon population 1930-2010.xlsx"
+  ) %>%
+  gather(year, y, -County, convert = TRUE) %>%
+  rename(county = County)
+
+devtools::use_data(historical_pop, overwrite = TRUE)
+
+
