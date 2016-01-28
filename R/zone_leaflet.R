ec2673ef57abc473d96e0777ba72106ae1cf3ed3
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-01-28 09:33:20 -0500

    Function to construct popup

--- R/zone_leaflet.R
+++ R/zone_leaflet.R
@@ -16,3 +16,27 @@ zone_leaflet <- function(shp){
 
 }
 
+
+#' Build popup tag for leaflet
+#'
+#' @inheritParams zone_leaflet
+#' @inheritParams change_leaflet
+#' @param var Variable to create popup tag for (e.g., \code{"pop"})
+#'
+change_popup <- function(shp, var, year1, year2){
+  # build popup
+  zone_info <- paste0("<strong>Alpha Zone: </strong>", shp@data$AZONE)
+
+  var_info <- paste0(
+    "<strong>", year1, " ", var, ": </strong>",
+    shp@data[, paste0("", var, "_", year1)], "<br>",
+    "<strong>", year2, " ", var, ": </strong>",
+    shp@data[, paste0("", var, "_", year2)], "<br>",
+    "<strong>Growth rate </strong>",
+    round(shp@data[, paste0(var, "_rate")], digits = 3), "%"
+  )
+
+  paste(zone_info, var_info, sep = "</br>")
+
+}
+
