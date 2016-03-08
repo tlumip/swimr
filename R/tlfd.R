f10ecb244700f68cbb4356ee32a6794536cdc42a
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-08 12:08:23 -0500

    Use TLFD from database
    Instead of building your own from a skim. Also add function for cumulative tlfd, close #19

--- R/tlfd.R
+++ R/tlfd.R
@@ -14,71 +14,36 @@
 #' @export
 extract_tlfd <- function(db,
                          region_var = c("MPO", "COUNTY", "STATE"),
-                         regions = NULL,
-                         bin_width = 1, max_bin = 100){
+                         regions = NULL){
 
-  # get travel distance skim between zones
-  skim <- tbl(db, "SKIM") %>%
-    left_join(
-      tbl(db, "BZONE") %>% select_("FROMBZONE" = "BZONE", region_var)
-    ) %>%
-    select_(
-      region_var, "FROMBZONE", "TOBZONE", "TSTEP",
-      "pk_dist" = "pkautodist",  "op_dist" = "opautodist"
-    )
+  region <- tbl(db, "ALLZONES") %>%
+    select_("azone" = "Azone", "region_var" = region_var)
 
   # if no regions specified, then keep all.
   if(!is.null(regions)){
     if(length(regions) == 1){
-      skim <- skim %>%
-        mutate_("region" = region_var) %>%
-        filter(region == regions)
+      region <- region %>%
+        filter(region_var == regions)
     } else {
-      skim <- skim %>%
-        mutate_("region" = region_var) %>%
-        filter(region %in% regions)
+      region <- region %>%
+        filter(region_var %in% regions)
     }
   }
 
-  # get number of auto trips between zones
-  trips <- tbl(db, "TRIPMATRIX") %>%
-    transmute(
-      FROMBZONE, TOBZONE, TSTEP,
-      pk_auto =
-        am_DA + am_SR2 + am_SR3P +
-        pm_DA + pm_SR2 + pm_SR3P,
-      op_auto =
-        md_DA + md_SR2 + md_SR3P +
-        nt_DA + nt_SR2 + nt_SR3P
-    )
-
-  tlfd <- inner_join(skim, trips) %>%
+  # get TLFD
+  tlfd <- tbl(db, "TLFD_SDT") %>%
+    left_join(region) %>%
+    group_by(region_var, TSTEP, distance) %>%
+    summarise(trips = sum(trips)) %>%
     collect() %>%
 
-    # bin distances
     mutate(
-      pk_dist = plyr::round_any(pk_dist, bin_width, floor),
-      pk_dist = pmin(pk_dist, max_bin),
-      op_dist = plyr::round_any(op_dist, bin_width, floor),
-      op_dist = pmin(op_dist, max_bin)
-    ) %>%
-
-    # reshape data
-    gather(var, value, pk_dist, op_dist, pk_auto, op_auto, convert = TRUE) %>%
-    separate(var, c("period", "var")) %>%
-    spread(var, value) %>%
-
-    # calculate number of autos in each distance bin in each year
-    group_by_(region_var, "TSTEP", "dist") %>%
-    summarize(auto = sum(auto)) %>%
-
-    # get frequency
-    group_by_(region_var, "TSTEP") %>%
-    mutate(
-      freq = auto / sum(auto),
+      freq = trips / sum(trips),
+      cum_freq = cumsum(freq),
       year = as.numeric(TSTEP) + 1990
     ) %>%
-    ungroup()
+    ungroup() %>%
+    select(-TSTEP)
 
 
   return(tlfd)
@@ -91,23 +56,24 @@ extract_tlfd <- function(db,
 #' @param db The scenario database.
 #' @param region_var The region to aggregate to.
 #' @param regions The regions to return.  If \code{NULL}, returns all.
-#' @param bin_width The width of bins to use in the trip length frequency
-#'   distribution. Defaults to 1.
-#' @param max_bin The maximum bin value. Defaults to 50.
+#' @param cumulative Plot cumulative instead of probability density distribution.
 #'
 #' @return A ggplot2 object.
 #'
 #' @export
 plot_tlfd <- function(db,
                       region_var = c("MPO", "COUNTY", "STATE"),
-                      regions = NULL,
-                      bin_width = 1, max_bin = 50){
+                      regions = NULL, cumulative = FALSE){
 
-  tlfd <- extract_tlfd(db, region_var, regions, bin_width, max_bin) %>%
-    mutate_("region_var" = region_var)
+  tlfd <- extract_tlfd(db, region_var, regions) %>%
+    filter(year == min(year) | year == max(year))
+
+  if(cumulative){
+    tlfd <- tlfd %>% mutate(freq = cum_freq)
+  }
 
   p <- ggplot(tlfd,
-         aes(x = dist, y = freq, group = year, color = year)) +
+         aes(x = distance, y = freq, group = year, color = year)) +
     geom_line(alpha = 0.5) +
     scale_color_gradient(low = "red", high = "blue") +
     facet_wrap(~ region_var) +
@@ -130,23 +96,28 @@ plot_tlfd <- function(db,
 #' @export
 compare_tlfd <- function(db1, db2,
                          region_var = c("MPO", "COUNTY", "STATE"),
-                         regions = NULL,
-                         bin_width = 1, max_bin = 50){
+                         regions = NULL, cumulative = FALSE){
 
-  tlfd1 <- extract_tlfd(db1, region_var, regions, bin_width, max_bin) %>%
-    mutate_("region_var" = region_var) %>%
-    select(region_var, year, dist, ref = freq)
-  tlfd2 <- extract_tlfd(db2, region_var, regions, bin_width, max_bin) %>%
-    mutate_("region_var" = region_var) %>%
-    select(region_var, year, dist, com = freq)
+  tlfd1 <- extract_tlfd(db1, region_var, regions)
+  tlfd2 <- extract_tlfd(db2, region_var, regions)
+
+  if(cumulative){
+    tlfd1 <- tlfd1 %>% mutate(ref = cum_freq)
+    tlfd2 <- tlfd2 %>% mutate(com = cum_freq)
+  } else {
+    tlfd1 <- tlfd1 %>% mutate(ref = freq)
+    tlfd2 <- tlfd2 %>% mutate(com = freq)
+  }
 
   df <- left_join(tlfd1, tlfd2) %>%
     mutate(
       diff = com - ref
-    )
+    ) %>%
+    filter(year == min(year) | year == max(year))
+
 
   p <- ggplot(df,
-              aes(x = dist, y = diff, group = year, color = year)) +
+              aes(x = distance, y = diff, group = year, color = year)) +
     geom_line(alpha = 0.5) +
     scale_color_gradient(low = "red", high = "blue") +
     facet_wrap(~ region_var) +
