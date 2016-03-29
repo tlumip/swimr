f6927aadfe5eb3942516d1e592e48e4a534a9966
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-29 11:10:15 -0400

    Refine tlfd functions

--- R/tlfd.R
+++ R/tlfd.R
@@ -3,8 +3,7 @@
 #' @param db The scenario database.
 #' @param region_var The region to aggregate to.
 #' @param regions The regions to return.  If \code{NULL}, returns all.
-#' @param bin_width The width of bins to use in the trip length frequency
-#'   distribution. Defaults to 1.
+#' @param years The years to show in the plot. If null, will show the first
 #' @param max_bin The maximum bin size to return.
 #'
 #' @return A data frame with the trip length frequency distribution by year
@@ -14,25 +13,19 @@
 #' @export
 extract_tlfd <- function(db,
                          region_var = c("MPO", "COUNTY", "STATE"),
-                         regions = NULL){
+                         regions = NULL, cumulative = FALSE, years = NULL){
 
   region <- tbl(db, "ALLZONES") %>%
     select_("azone" = "Azone", "region_var" = region_var)
 
   # if no regions specified, then keep all.
   if(!is.null(regions)){
-    if(length(regions) == 1){
-      region <- region %>%
-        filter(region_var == regions)
-    } else {
-      region <- region %>%
-        filter(region_var %in% regions)
-    }
+    region <- region %>% filter(region_var %in% regions)
   }
 
   # get TLFD
   tlfd <- tbl(db, "TLFD_SDT") %>%
-    left_join(region) %>%
+    inner_join(region) %>%
     group_by(region_var, TSTEP, distance) %>%
     summarise(trips = sum(trips)) %>%
     collect() %>%
@@ -44,6 +37,18 @@ extract_tlfd <- function(db,
     ) %>%
     ungroup() %>%
     select(-TSTEP)
+
+  # filter to the years you want.
+  if(is.null(years)){
+    tlfd <- tlfd %>% filter(year == min(year) | year == max(year))
+  } else {
+    tlfd <- tlfd %>% filter(year %in% years)
+  }
+
+  # return cumulative if wanted
+  if(cumulative){
+    tlfd <- tlfd %>% mutate(freq = cum_freq)
+  }
 
 
   return(tlfd)
@@ -57,25 +62,23 @@ extract_tlfd <- function(db,
 #' @param region_var The region to aggregate to.
 #' @param regions The regions to return.  If \code{NULL}, returns all.
 #' @param cumulative Plot cumulative instead of probability density distribution.
+#' @param years The years to show in the plot. If null, will show the first
+#'   and last years.
 #'
 #' @return A ggplot2 object.
 #'
 #' @export
 plot_tlfd <- function(db,
                       region_var = c("MPO", "COUNTY", "STATE"),
-                      regions = NULL, cumulative = FALSE){
+                      regions = NULL, cumulative = FALSE, years = NULL){
 
-  tlfd <- extract_tlfd(db, region_var, regions) %>%
-    filter(year == min(year) | year == max(year))
+  tlfd <- extract_tlfd(db, region_var, regions, cumulative, years)
 
-  if(cumulative){
-    tlfd <- tlfd %>% mutate(freq = cum_freq)
-  }
-
-  p <- ggplot(tlfd,
-         aes(x = distance, y = freq, group = year, color = year)) +
-    geom_line(alpha = 0.5) +
-    scale_color_gradient(low = "red", high = "blue") +
+  p <-
+    ggplot(tlfd,
+         aes(x = distance, y = freq, group = factor(year), color = factor(year))) +
+    geom_line() +
+    scale_color_discrete("Year") +
     facet_wrap(~ region_var) +
     ylab("Frequency") + xlab("Miles") +
     theme_bw() + theme(axis.text.x = element_text(angle = 30))
@@ -96,30 +99,23 @@ plot_tlfd <- function(db,
 #' @export
 compare_tlfd <- function(db1, db2,
                          region_var = c("MPO", "COUNTY", "STATE"),
-                         regions = NULL, cumulative = FALSE){
+                         regions = NULL, cumulative = FALSE, years = NULL){
 
-  tlfd1 <- extract_tlfd(db1, region_var, regions)
-  tlfd2 <- extract_tlfd(db2, region_var, regions)
-
-  if(cumulative){
-    tlfd1 <- tlfd1 %>% mutate(ref = cum_freq)
-    tlfd2 <- tlfd2 %>% mutate(com = cum_freq)
-  } else {
-    tlfd1 <- tlfd1 %>% mutate(ref = freq)
-    tlfd2 <- tlfd2 %>% mutate(com = freq)
-  }
+  tlfd1 <- extract_tlfd(db1, region_var, regions, cumulative, years) %>%
+    rename(ref = freq)
+  tlfd2 <- extract_tlfd(db2, region_var, regions, cumulative, years) %>%
+    rename(com = freq)
 
   df <- left_join(tlfd1, tlfd2) %>%
-    mutate(
-      diff = com - ref
-    ) %>%
-    filter(year == min(year) | year == max(year))
+    mutate(diff = com - ref)
 
 
-  p <- ggplot(df,
-              aes(x = distance, y = diff, group = year, color = year)) +
+  p <- ggplot(
+    df,
+    aes(x = distance, y = diff, group = factor(year), color = factor(year))
+  ) +
     geom_line(alpha = 0.5) +
-    scale_color_gradient(low = "red", high = "blue") +
+    scale_color_discrete("Year") +
     facet_wrap(~ region_var) +
     ylab("Difference in Frequency (reference - current)") + xlab("Miles") +
     theme_bw() + theme(axis.text.x = element_text(angle = 30))
