0182a3cd1bc318352349fa3b2fbbfd7b78bae340
Greg Macfarlane <macfarlaneg@pbworld.com>
2016-11-07 13:01:46 -0800

    Allow employment types to use custom employment types

--- R/employment.R
+++ R/employment.R
@@ -8,12 +8,13 @@
 #' @param facet_levels The levels of the facet variable to keep. Defaults to all
 #'   levels other than external stations.
 #' @param type_levels The types of employment to show in the plot.
+#' @param employment_categories [Optional] data_frame of employment aggregation categories;
+#'   if \code{NULL}, uses \code{\link{emp_types}}. Should have columns
+#'   \code{ACTIVITY} (full code of activity in AA databases) and \code{emp_type}.
 #'
 #' @export
-extract_employment <- function(db,
-                               facet_var = c("MPO", "COUNTY", "STATE"),
-                               facet_levels = NULL,
-                               type_levels = NULL){
+extract_employment <- function(db, facet_var = NULL, facet_levels = NULL,
+                               type_levels = NULL, employment_categories = NULL){
 
   # set facet variable; if null then default to MPO
   if(is.null(facet_var)){
@@ -31,10 +32,6 @@ extract_employment <- function(db,
     facet_levels <- facet_levels[which(facet_levels != "EXTSTA")]
   }
 
-  # get levels of employment type
-  if(is.null(type_levels)){
-    type_levels <- employment_types$naics1
-  }
 
   employment <- dplyr::tbl(db, "ActivityLocations") %>%
     dplyr::select(BZONE, ACTIVITY, TSTEP, Employment) %>%
@@ -45,17 +42,29 @@ extract_employment <- function(db,
     dplyr::filter(facet_var %in% facet_levels) %>%
     dplyr::group_by(facet_var, ACTIVITY, TSTEP) %>%
     dplyr::summarize(emp = sum(Employment)) %>%
+    dplyr::mutate(year = as.numeric(TSTEP) + 1990) %>%
     dplyr::ungroup() %>%
-    dplyr::collect(n=Inf) %>%
+    dplyr::collect(n=Inf)
 
-    # consolidate employment categories
-    dplyr::mutate(
-      ACTIVITY =  gsub("_.*", "", ACTIVITY),
-      year = as.numeric(TSTEP) + 1990
-    ) %>%
-    dplyr::left_join(emp_types, by = "ACTIVITY") %>%
-    dplyr::group_by(facet_var, emp_type, year) %>%
-    dplyr::summarize(emp = sum(emp))
+  # consolidate employment categories
+  if(is.null(employment_categories)){ # use included aggregation
+    employment <-
+      employment %>%
+      dplyr::mutate(ACTIVITY =  gsub("_.*", "", ACTIVITY)) %>%
+      dplyr::left_join(emp_types, by = "ACTIVITY") %>%
+      dplyr::group_by(facet_var, emp_type, year) %>%
+      dplyr::summarize(emp = sum(emp))
+  } else {  # use custom aggregation
+    employment <- employment %>%
+      dplyr::left_join(employment_categories, by = "ACTIVITY") %>%
+      dplyr::group_by(facet_var, emp_type, year) %>%
+      dplyr::summarize(emp = sum(emp))
+  }
+
+  if(!is.null(type_levels)){
+    employment <- employment %>%
+      dplyr::filter(emp_type %in% type_levels)
+  }
 
 
 
@@ -67,21 +76,17 @@ extract_employment <- function(db,
 #' This function plots the employment by type in an area over time between two
 #' scenarios.
 #'
-#' @param db The scenario database.
-#' @param facet_var The variable in the zone table to facet by. Defaults to MPO
-#' @param facet_levels The levels of the facet variable to keep. Defaults to all
-#'   levels other than external stations.
-#' @param type_levels The types of employment to show in the plot.
+#' @inheritParams extract_employment
 #'
 #' @return A ggplot2 object showing the employment by type and and year.
 #'
 #' @export
-plot_employment <- function(db,
-                          facet_var = c("MPO", "COUNTY", "STATE"),
-                          facet_levels = NULL,
-                          type_levels = NULL){
+plot_employment <- function(db, facet_var = "MPO",
+                            facet_levels = NULL, type_levels = NULL,
+                            employment_categories = NULL){
 
-  employment <- extract_employment(db, facet_var, facet_levels, type_levels)
+  employment <- extract_employment(db, facet_var, facet_levels, type_levels,
+                                   employment_categories)
 
   # make plot
   ggplot2::ggplot(employment,
@@ -105,25 +110,22 @@ plot_employment <- function(db,
 #'
 #' @param db1 The reference scenario database.
 #' @param db2 The current scenario database.
-#' @param facet_var The variable in the zone table to facet by. Defaults to MPO
-#' @param facet_levels The levels of the facet variable to keep. Defaults to all
-#'   levels other than external stations.
-#' @param type_levels The types of employment to show in the plot.
+#' @inheritParams extract_employment
 #'
 #' @return A ggplot2 object showing the employment by type and and year.
 #'
 #' @export
-compare_employment <- function(db1, db2,
-                            facet_var = c("MPO", "COUNTY", "STATE"),
-                            facet_levels = NULL,
-                            type_levels = NULL){
+compare_employment <- function(db1, db2, facet_var = "MPO", facet_levels = NULL,
+                               type_levels = NULL, employment_categories = NULL){
 
   # get the reference scenario data
-  fref <- extract_employment(db1, facet_var, facet_levels, type_levels) %>%
+  fref <- extract_employment(db1, facet_var, facet_levels, type_levels,
+                             employment_categories) %>%
     dplyr::rename(ref = emp)
 
   # get the comparison scenario
-  fcom <- extract_employment(db2, facet_var, facet_levels, type_levels) %>%
+  fcom <- extract_employment(db2, facet_var, facet_levels, type_levels,
+                             employment_categories) %>%
     dplyr::rename(com = emp)
 
   f <- dplyr::left_join(fref, fcom) %>%
