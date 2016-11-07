ed432398a8ac96e84fbb2f7c88bd1191a4e9f6b6
Greg Macfarlane <macfarlaneg@pbworld.com>
2016-11-07 09:59:38 -0800

    Only use beta zones
    At this point we will assume a 1:many mapping between Alpha zone collections and beta zones.

--- R/add_lookup.R
+++ R/add_lookup.R
@@ -3,8 +3,8 @@
 #' @param db A SWIM scenario database
 #' @param df A lookup table with two colums:
 #' \enumerate{
-#'   \item{A zone field, which should be named either \code{Azone} or
-#'   \code{BZONE}, depending on the level of the zone aggregation.}
+#'   \item{A beta zone field \code{BZONE}, depending on the level of the zone
+#'   aggregation.}
 #'   \item{An aggregation field, which will have some value for all zones. The
 #'   name of the data column will be the name of field in the table.}
 #' }
@@ -15,19 +15,24 @@
 #'
 add_lookup <- function(db, df){
 
-  tbl <- tbl(db, "ALLZONES")
+  allzones <- tbl(db, "ALLZONES")
+  bzones <- tbl(db, "BZONE")
 
   # check if field already exists
   new_field <- names(df)[2]
-  if(new_field %in% names(tbl)){
+  if(new_field %in% names(allzones) | new_field %in% names(bzones)){
     stop("Field already exists")
   } else {
-    tbl <- tbl %>%
-      collect() %>%
-      left_join(df)
+    # add to beta zones
+    bzones <- bzones %>% collect() %>% left_join(df, by = "BZONE")
+    db_drop_table(db$con, "BZONE", force = TRUE)
+    copy_to(db, bzones, name = "BZONE", temporary = FALSE)
+
+    # add to alzones
+    allzones <- allzones %>% collect() %>% left_join(df, by = "BZONE")
+    db_drop_table(db$con, "ALLZONES", force = TRUE)
+    copy_to(db, allzones, name = "ALLZONES", temporary = FALSE)
   }
 
-  db_drop_table(db$con, "ALLZONES", force = TRUE)
-  copy_to(db, tbl, name = "ALLZONES", temporary = FALSE)
 
 }
